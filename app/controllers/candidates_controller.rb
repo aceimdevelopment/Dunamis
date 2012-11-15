33c0c31723fc429e17abd14884bbe0805e8fc151
danielousky <danielousky@gmail.com>
2012-11-15 17:55:02 -0430

    Avances en la importación

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -3,7 +3,8 @@ class CandidatesController < ApplicationController
   # GET /candidates.json
   def index
     @candidates = Candidate.all
-
+    import
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @candidates }
@@ -80,4 +81,88 @@ class CandidatesController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  
+  # ===================== DEFINIDAS INTERNAMENTE =============================== #
+  
+  def import
+    require 'uri'
+    require 'mechanize'
+  
+    cunas_fichas_url = "http://sigecup.cne.gob.ve/index.php/cunas_en_vivo/consultar/por_ficha"
+    aparicion_cunas  = "http://sigecup.cne.gob.ve/index.php/cunas_en_vivo/reportes/aparicion_de_cunas"
+  
+    a = Mechanize.new
+    page = login_sigecup a
+    page2 = page.link_with(:href => cunas_fichas_url).click
+
+    page2_form = page2.forms.first
+    page2_form.fields.each { |f| puts f.name }
+    puts "Inicio......"
+    page2_form.limit = -1
+    page3 = a.submit(page2_form, page2_form.buttons.first)
+  
+    page3_form2 = page3.search("table")[2]
+    puts page3_form2
+    rows = page3_form2.search("tr")
+    rows.each do |tr|
+      tds = tr.search("td")
+      puts "num: #{tds[1].get_attribute('a')}"
+      
+    end
+    # puts page3_form2
+    
+    
+    # 
+    # <td>5</td>
+    # <td id="NroCuna_column_4"><a href="http://sigecup.cne.gob.ve/index.php/cunas_en_vivo/consultar/registro/172">CUÑA-0025</a></td>
+    # <td id="Fecha_column_4">13-11-2012</td>
+    # <td id="Tiempo_column_4">30 seg.</td>
+    # <td id="NombreClave_column_4"><a href="http://sigecup.cne.gob.ve/index.php/cunas_en_vivo/consultar/registro/172">EJaua PSUV Marta González Prod Cacao</a></td>
+    # <td id="Grupo_column_4"><a href="http://sigecup.cne.gob.ve/index.php/cunas_en_vivo/grupo_de_cuna/registro/1">Sin Grupo</a></td>
+    # <td id="Partido_column_4" style="display: none;"><a href="http://sigecup.cne.gob.ve/index.php/political_party/political_party_controller/show_political_party/2">Psuv</a></td>
+    # <td id="Tolda_column_4">Chavismo</td>
+    # <td id="Adj_column_4">1</td>
+    # <td id="Ilicitos_column_4">No</td>
+    # <td id="Voceros_column_4" style="display: none;"> </td>
+    # <td id="Candidatos_column_4" style="display: none;"><a href="http://sigecup.cne.gob.ve/index.php/general/political_representative_controller/show_political_representative/8" title="Ver datos de vocero">Elías  Jaua</a></td>
+    
+    
+    # ======================= VIEW ========================#
+    
+    
+    
+    
+    
+      
+  end
+  
+  def login_sigecup a
+    username = "DMOROS"
+    password = "dm893585"
+    url = URI.parse('http://sigecup.cne.gob.ve')
+  
+    index = a.get(url)
+    # captura de Imagen Captcha
+    img = index.search("img")
+    puts "#{img}"
+  
+    puts "Introduzca el Valor del Captcha:"
+    captcha = gets.chomp # lectura por consola de valor Captcha
+  
+  
+    # captura de form login
+    login_form = index.forms.first
+  
+    login_form.login = username
+    login_form.password = password
+    login_form.captcha = captcha
+  
+    # Carga de principal de la aplicación
+    a.submit(login_form, login_form.buttons.first)
+  end
+  
+  
+  
 end
+
--- lib/importer.rb
+++ lib/importer.rb
@@ -1,47 +1,50 @@
 # coding: utf-8
 module Importer
-  def cadidates
-    require "uri"
+  def self.candidates
+    require 'uri'
     require 'mechanize'
-
+  
+    cunas_fichas_url = "http://sigecup.cne.gob.ve/index.php/cunas_en_vivo/consultar/por_ficha"
+    aparicion_cunas  = "http://sigecup.cne.gob.ve/index.php/cunas_en_vivo/reportes/aparicion_de_cunas"
+  
     a = Mechanize.new
     page = login_sigecup a
-    page2 = page.link_with(:href => "http://sigecup.cne.gob.ve/index.php/general/political_representative_controller/show_all_political_representatives").click
+    page2 = page.link_with(:href => cunas_fichas_url).click
 
     page2_form = page2.forms.first
     page2_form.fields.each { |f| puts f.name }
-    
+    puts "Inicio......"
     page2_form.limit = -1
     page3 = a.submit(page2_form, page2_form.buttons.first)
-    
+  
     page3_form2 = page3.search("table")[2]
-        
+  
+    puts page3_form2
+      
   end
   
-  def login_sigecup a
+  def self.login_sigecup a
     username = "DMOROS"
     password = "dm893585"
     url = URI.parse('http://sigecup.cne.gob.ve')
-    
+  
     index = a.get(url)
     # captura de Imagen Captcha
     img = index.search("img")
     puts "#{img}"
-    
+  
     puts "Introduzca el Valor del Captcha:"
     captcha = gets.chomp # lectura por consola de valor Captcha
-    
-    
+  
+  
     # captura de form login
     login_form = index.forms.first
-    
+  
     login_form.login = username
     login_form.password = password
     login_form.captcha = captcha
-    
+  
     # Carga de principal de la aplicación
     a.submit(login_form, login_form.buttons.first)
-  end
-  
-  private
+  end  
 end
