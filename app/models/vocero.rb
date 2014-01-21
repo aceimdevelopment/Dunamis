42c3e7464afab9f2f20a57895058f1e8b9103f81
danielousky <danielousky@gmail.com>
2014-01-21 11:49:23 -0430

    Inclusion de foto en el buscador de vocero

--- app/models/vocero.rb
+++ app/models/vocero.rb
@@ -8,4 +8,17 @@ class Vocero < ActiveRecord::Base
   attr_accessible :descripcion, :foto, :nombre, :organizacion_id, :tipo_cargo_id
   validates :nombre, uniqueness: { case_sensitive: false }
   
+  def sin_foto
+    foto = "#{Rails.root}/images/sin_foto.png" if foto.blank? 
+  end
+  
+  def descripcion_completa
+    
+    # foto = asset_path 'sin_foto.png' if foto.blank?
+    "<img class='flag' src='#{foto}' style='max-width: 50px; max-height:50px;'/> #{nombre}"
+  end   
+  
+  
+  
+  
 end
