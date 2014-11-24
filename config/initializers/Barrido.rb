44a3b84bba0a12740ee94ebede715cc3aa36fa1d
danielousky <danielousky@gmail.com>
2014-11-24 13:17:11 -0430

    Pruebas en la ejecucion de tareas en el arranque

--- config/initializers/Barrido.rb
+++ config/initializers/Barrido.rb
@@ -1,14 +1,14 @@
-$stdout.puts "Barriendo..."
-begin
-  
-  job1 = fork do
-  exec "RAILS_ENV=production rake importar_notas_website"
-  end
-
-  Process.detach(job1)
-  system 'echo "post parrido..."'
-  
-rescue 
-  puts "** Oops! No se pudo importar! **"
-end
-$stdout.puts "fin de barrido"
+# $stdout.puts "Barriendo..."
+# begin
+#   
+#   job1 = fork do
+#   exec "RAILS_ENV=production rake importar_notas_website"
+#   end
+# 
+#   Process.detach(job1)
+#   system 'echo "post parrido..."'
+#   
+# rescue 
+#   puts "** Oops! No se pudo importar! **"
+# end
+# $stdout.puts "fin de barrido"
