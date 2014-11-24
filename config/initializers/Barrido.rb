ad2d7994abe1c6e88286462e2fd18fce320479a8
danielousky <danielousky@gmail.com>
2014-11-24 11:09:50 -0430

    Prueba para el levantamiento desde el servidor de tareas

--- config/initializers/Barrido.rb
+++ config/initializers/Barrido.rb
@@ -1,7 +1,7 @@
-# $stdout.puts "Barriendo..."
-# begin
-#   `rake importar_notas_website`
-# rescue 
-#   puts "** Oops! No se pudo importar! **"
-# end
-# $stdout.puts "fin de barrido"
+$stdout.puts "Barriendo..."
+begin
+  `RAILS_ENV=production rake importar_notas_website`
+rescue 
+  puts "** Oops! No se pudo importar! **"
+end
+$stdout.puts "fin de barrido"
