faa9e0bcb1eb232c05a6ee9c30d74fa1dc34253c
danielousky <danielousky@gmail.com>
2016-09-09 15:31:27 -0430

    habilitacion de paso2 guardar

--- app/controllers/wizard_controller.rb
+++ app/controllers/wizard_controller.rb
@@ -96,7 +96,7 @@ class WizardController < ApplicationController
   end
 
   def paso2_guardar
-    1/0
+
     #manejo de website activa mediante el uso de la sesion
     session[:website_activa] = params[:website_activa]
     
