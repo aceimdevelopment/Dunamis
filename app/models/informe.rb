3189aeab3e2cada395ff4bd3765bb49649064dd0
danielousky <danielousky@gmail.com>
2016-09-12 11:21:45 -0430

    Ajustes en las dependencias

--- app/models/informe.rb
+++ app/models/informe.rb
@@ -1,11 +1,11 @@
 class Informe < ActiveRecord::Base
   attr_accessible :fecha, :resumen, :autor, :tema, :titulo
   
-  has_many :resumenes
-  accepts_nested_attributes_for :resumenes, :dependent => :destroy
+  has_many :resumenes, :dependent => :destroy
+  accepts_nested_attributes_for :resumenes
 
-  has_many :informes_temas
-  accepts_nested_attributes_for :informes_temas, :dependent => :destroy
+  has_many :informes_temas, :dependent => :destroy
+  accepts_nested_attributes_for :informes_temas
   
   # has_and_belongs_to_many :temas, :join_table => "informes_temas"
   
